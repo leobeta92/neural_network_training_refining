0361c0dd2e06d6b87fce2efeaa0848ee64b3bd71
leobeta92 <leobeta92@gmail.com>
2026-09-22 18:42:35 +0200

    Adding progress of models here. Trying to attain a score of 80% on test data. Doing a lot of repetitive work, so will be fleshing out src folder of functions...

---
+++ src/utils_plots.py
@@ -0,0 +1,47 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+def plot_cross_entropy_loss(history):
+    plt.title('Cross Entropy Loss')
+    plt.plot(history.history['loss'], color='blue', label='train')
+    plt.plot(history.history['val_loss'], color='orange', label='val')
+    plt.legend()
+    plt.show()
+
+def plot_classification_accuracy(history):
+    plt.title('Classification Accuracy')
+    plt.plot(history.history['accuracy'], color='blue', label='train')
+    plt.plot(history.history['val_accuracy'], color='orange', label='val')
+    plt.legend()
+    plt.show()
+
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(predicted_label,
+                                100*np.max(predictions_array),
+                                true_label),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array, true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
